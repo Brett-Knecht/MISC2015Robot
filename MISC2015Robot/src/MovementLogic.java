867db8b4959573b03c92ef40fb1152aec4a4096d
Grayson Lorenz <lunar_furor@hotmail.com>
2015-02-07 05:19:37 -0600

    First Commit
    First upload of the files.

---
+++ MISC2015Robot/src/MovementLogic.java
@@ -0,0 +1,25 @@
+/**
+ * This file will handle any extra movement logic we may need. 
+ * This is where finding the line with sensors, rotation to line up, and other fancy business can be done.
+ * @author Grayson Lorenz
+ *
+ */
+public class MovementLogic {
+	//////////////////////////////////////////////
+	//FIELDS//////////////////////////////////////
+	WheelDriver wheels;
+	
+	//CONSTRUCTORS////////////////////////////////
+	public MovementLogic(){
+		
+	}
+	
+	//ACTION METHODS//////////////////////////////
+	
+	
+	//GETTERS AND SETTERS/////////////////////////
+	public static String ToString(){
+		return "MOVEMENT: output";
+	}
+	
+}
